9cd3a2cc4cc405226dffecbc355b2d13187101b9
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-04-19 01:51:41 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import NewItems from './NewItems'
+
+export default class News extends Component {
+  constructor(){
+    super();
+    console.log("Hi I am a constructor!");
+    this.state={
+      articles:[],
+      page:1,
+      loading:false
+    }
+  }
+
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0";
+    let data = await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData)
+    this.setState({
+      articles:parsedData.articles
+    })
+
+  }
+  render() {
+    return (
+      <>
+      <div className='container my-5'>
+        <h2>News Vitals- Get your daily news here!</h2>
+        <div className='row'>
+          {
+            this.state.articles.map((element)=>{
+              return <div className='col-md-4' key={element.url}>
+                <NewItems/>
+              </div>
+            })
+          }
+        </div>
+      </div>
+      </>
+    )
+  }
+}
