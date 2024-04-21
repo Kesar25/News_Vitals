8628e15c393ba0e6610abb7ce80016dd43cb185c
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-04-21 13:42:57 +0530

    Adding an Infinite Scroller

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NewItems from './NewItems'
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   constructor(){
@@ -9,55 +10,58 @@ export default class News extends Component {
     this.state={
       articles:[],
       page:1,
-      loading:false
+      loading:true,
+      totalResults:0
     }
   }
 
-  async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0&category=${this.props.category}`;
-    this.setState({loading:true});
+ 
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0&category=${this.props.category}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
-    let parsedData=await data.json();
-    console.log(parsedData)
+    let parsedData = await data.json() 
+    
     this.setState({
-      articles:parsedData.articles,
-      loading:false
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false
+       
     })
 
-  }
+}
+    async componentDidMount(){
+      this.updateNews()
+    }
 
-  handleNextClick=async ()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0&page=${this.state.page+1}&category=${this.props.category}`;
-    this.setState({loading:true})
-    let data=await fetch(url);
-    let parsedData=await data.json();
+ 
+  fetchMoreData = async () => {  
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0&category=${this.props.category}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
     this.setState({
-      articles:parsedData.articles,
-      page:this.state.page+1,
-      loading:false
+        articles: this.state.articles.concat(parsedData.articles),
+        totalResults: parsedData.totalResults
     })
-  }
-  handlePrevClick=async ()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=3ddfee9bf286413595b490c0fae359d0&page=${this.state.page-1}&category=${this.props.category}`;
-    this.setState({loading:true})
-    let data=await fetch(url);
-    let parsedData=await data.json();
-    this.setState({
-      articles:parsedData.articles,
-      page:this.state.page-1,
-      loading:false
-    })
-  }
+  };
+  
   render() {
     let {category} =this.props
     return (
       <>
-      {this.state.loading && <Spinner/>}
       <div className='container my-3'>
         <h2>News Vitals- Get your daily dose of news here!</h2>
-        
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+        >
+        <div className='container'>
         <div className='row'>
-          {!this.state.loading && 
+          {
             this.state.articles.map((element)=>{
               return <div className='col-md-3' key={element.url}>
                 <NewItems title={element.title} url={element.url} imgUrl={element.urlToImage} desc={element.description}/>
@@ -65,10 +69,9 @@ export default class News extends Component {
             })
           }
         </div>
-        <div className="container d-flex justify-content-between">
-        <button type="button" disabled={this.state.page<=1} onClick={this.handlePrevClick} className="btn btn-dark "> &larr; Previous</button>
-        <button type="button"  onClick={this.handleNextClick} className="btn btn-dark ">Next &rarr;</button>
         </div>
+        
+        </InfiniteScroll>
       </div>
       
       </>
